1f5fb971bbe6f5a364d0a52609fac40bcbdafd8e
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-01-06 22:58:09 +0000

    Add files via upload
    Original code for chosen example of JavaScript art

--- jsartoriginal.js
+++ jsartoriginal.js
@@ -1,41 +1,42 @@
-var np = 300;
-var startcol;
-
 function setup() {
-	createCanvas(1366, 600);
-	background(255);
-	noFill();
-	noiseSeed(random(100));
-	startcol = random(255);
+background(255);
+createCanvas(windowWidth,windowHeight);  
+}  
+
+var PY, PX;
+var x = [20];
+var y = [20];
+var segLength = 5;
+
+for(var i=0; i<60; i++) {
+    x[i]=1;
+    y[i]=1;
 }
+
+function segment( x, y,  a) {
+  strokeWeight(1);
+  stroke(0, 0, 0,50);
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+};
+
+function dragSegment( i,  xin,  yin) {
+  var dx = xin - x[i];
+  var dy = yin - y[i];
+  var angle = atan2(dy, dx);  
+  x[i] = xin - cos(angle) * segLength;
+  y[i] = yin - sin(angle) * segLength;
+  segment(x[i], y[i], angle);
+};
 
 function draw() {
-	// background(51);
-	beginShape();
-	var sx, sy;
-	for(var i = 0; i < np; i++) {
-		var angle = map(i, 0, np, 0, TWO_PI);
-		var cx = frameCount * 2 - 200;
-		var cy = height / 2 + 50 * sin(frameCount / 50);
-		var xx = 100 * cos(angle + cx / 10);
-		var yy = 100 * sin(angle + cx / 10);
-		var v = createVector(xx, yy);
-		xx = (xx + cx) / 150; yy = (yy + cy) / 150;
-		v.mult(1 + 1.5 * noise(xx, yy));
-		vertex(cx + v.x, cy + v.y);
-		if(i == 0) {
-			sx = cx + v.x;
-			sy = cy + v.y;
-		}
-	}
-	colorMode(HSB);
-	var hue = cx / 10 - startcol;
-	if(hue < 0) hue += 255;
-	stroke(hue, 100, 120);
-	strokeWeight(0.1);
-	vertex(sx, sy);
-	endShape();
-	if(frameCount > width + 500) {
-		noLoop();
-	}
-}
+ PX=mouseX;
+ PY=mouseY;
+  dragSegment(0, PX, PY);
+  for(var i=0; i<x.length-1; i++) {
+    dragSegment(i+1, x[i], y[i]);
+  }
+};
