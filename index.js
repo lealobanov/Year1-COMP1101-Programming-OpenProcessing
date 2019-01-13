e46fcf62de1dc18523eda7d5abb62480d16a5ab2
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-01-13 12:26:11 +0000

    Added form controls

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function draw(obj) {
       art.draw()
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    let cc = document.getElementById("colour");
+    function changeColour(event){
+    let colour = document.getElementById("colour").value;
+    art.setColour(colour);
+    }
+
+
+    let sl = document.getElementById("segLength");
+    function changeSegLength(event){
+    let segLength = document.getElementById("segLength").value;
+    art.setSegmentLength(segLength);
+    }
+    
+    cc.addEventListener("change", changeColour);
+    sl.addEventListener("input", changeSegLength);
+ 
+
+    let customize = document.getElementById("customize");
+
+    customize.addEventListener("submit", function (event){
+    event.preventDefault()});
+});
