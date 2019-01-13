86595c4e8d30035515023ef79b9ba0ecc4efb9e3
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-01-13 11:16:37 +0000

    Added get/set methods to be called using form controls, added functionality for optional p5 renderer

--- art_refactor.js
+++ art_refactor.js
@@ -1,75 +1,85 @@
-let art;
-
-function setup() {
-  createCanvas(windowWidth,windowHeight); 
-  background('#000000')
-  art = new SmokeBrush('#FFFFFF') 
-  art.iterate()
-
-}
-
-function draw() {
-  art.draw()
-  }
-
 class SmokeBrush {
-  constructor(coloring ='#FF0000' , segLength=5, curvature=60, x=[20],y=[20]) {
-    this.coloring = coloring;
-    this.segLength = segLength; 
-    this.curvature = curvature;
-    this._x = x;
-		this._y = y;	
+    constructor(colour ='FFFFFF' , segLength=5, curvature=60, x=[20],y=[20]) {
+        this.colour = colour;
+        this.segLength = segLength; 
+        this.curvature = curvature;
+        this._x = x;
+        this._y = y;	
+    }
 
-	}
+    set newColour(colour){
+        this.colour = colour;
+    }
 
-  get x(){
-    return this._x
-  }
+    set newSegmentLength(segLength){
+        this.segLength = segLength;    
 
-  get y(){
-    return this._y
-  }
+    }
 
-  iterate(){
-    for(let i=0; i<this.curvature; i++) {
-      this.x[i]=1;
-      this.y[i]=1;
-    }  
-  }
+    set newCurvature(curvature){
+        this.curvature = curvature
+    }
 
-	segment(x, y, a) {
-      strokeWeight(1);
-      stroke(this.coloring);
-      push();
-      translate(x, y);
-      rotate(a);
-      line(0, 0, this.segLength, 0);
-      pop();
-  }
+    set x(x){
+        this._x = x;
+    }
 
-	dragSegment( i,  xin,  yin) { 
+    get x(){
+        return this._x;
+    }
+
+    set y(y){
+        this._y =y;
+    }
+
+    get y(){
+        return this._y;
+    }
+
+    iterate(){
+        for(let i=0; i<this.curvature; i++) {
+            this.x[i]=1;
+            this.y[i]=1;
+        }  
+    }
+
+    segment(x, y, a) {
+        strokeWeight(1);
+        stroke(this.colour);
+        push();
+        translate(x, y);
+        rotate(a);
+        line(0, 0, this.segLength, 0);
+        pop();
+    }
+
+    dragSegment( i,  xin,  yin) { 
  		let dx = xin - this.x[i];
   	let dy = yin - this.y[i];
   	let angle = atan2(dy, dx);  
   		this.x[i] = xin - cos(angle) * this.segLength;
   		this.y[i] = yin - sin(angle) * this.segLength;
-  		return this.segment(this.x[i], this.y[i], angle);
+  		
+        return this.segment(this.x[i], this.y[i], angle);
   	}
 
-  draw() {
-    if(mouseIsPressed && mouseButton == LEFT){
-      let PX = mouseX;
-      let PY= mouseY;
-  	  this.dragSegment(0, PX, PY)
-      for(let i=0; i<this.x.length-1; i++) {
-        this.dragSegment(i+1, this.x[i], this.y[i]);
-      }
+    draw() {
+        if(mouseIsPressed && mouseButton == LEFT){
+            let PX = mouseX;
+            let PY= mouseY;
+  	  this.dragSegment(0, PX, PY);
+            for(let i=0; i<this.x.length-1; i++) {
+                this.dragSegment(i+1, this.x[i], this.y[i]);
+            }
+        }
+        if(mouseIsPressed && mouseButton == RIGHT){
+            background('#000000');  
+     }   
     }
-    if(mouseIsPressed && mouseButton == RIGHT){
-      background('#000000')
+  
     }
-  }
-}
+
+
 
 /*
  "Smoke Brush" by Laura Valentini
