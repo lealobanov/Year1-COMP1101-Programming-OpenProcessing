c4a37b703c95e30d654c174a08b57dba648977f9
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-01-15 23:49:55 +0000

    Update art_refactor.js

--- art_refactor.js
+++ art_refactor.js
@@ -1,44 +1,64 @@
 class SmokeBrush {
-    constructor(colour ='FFFFFF', segLength=5, curvature=60, x=[20],y=[20]) {
+    constructor(colour ='#B0E0E6', segLength=7, strokeWght=1, xcurve=0.15, ycurve=0, curvature=60, x=[20],y=[20]) {
         this.colour = colour;
         this.segLength = segLength; 
-        this._curvature = curvature;
-        this._x = x;
-        this._y = y;	
+        this.strokeWght = strokeWght;
+        this.xcurve = xcurve;
+        this.ycurve =ycurve;
+        this.curvature = curvature;
+        this.x = x;
+        this.y = y;	
     }
 
+    getColour(){
+        return this.colour;
+    }
     setColour(colour){
         this.colour = colour;
     }
-
+    getSegmentLength(){
+        return this.segLengthr;
+    }   
     setSegmentLength(segLength){
         this.segLength = segLength;    
-
+    }
+    getStrokeWght(){
+        return this.strokeWght;
+    }
+    setStrokeWght(strokeWght){
+        this.strokeWght = strokeWght;
+    }
+    getXcurve(){
+        return this.xcurve;
+    }
+    setXcurve(xcurve){
+        this.xcurve = xcurve;
+    }
+    getYcurve(){
+        return this.ycurve;
+    }  
+    setYcurve(ycurve){
+        this.ycurve = ycurve;
+    }
+    getCurvature() {
+        return this.curvature;
+    }
+    setCurvature(curvature){
+        this.curvature = curvature;
+    }
+    getX(){
+        return this.x;
+    }
+    setX(x){
+        this.x = x;
+    }
+    getY(){
+        return this.y;
+    }  
+    setY(y){
+        this.y =y;
     }
 
-    set curvature(curvature){
-        this._curvature = curvature;
-    }
-
-    get curvature() {
-        return this._curvature;
-    }
-
-    set x(x){
-        this._x = x;
-    }
-
-    get x(){
-        return this._x;
-    }
-
-    set y(y){
-        this._y =y;
-    }
-
-    get y(){
-        return this._y;
-    }
 
     iterate(){
         for(let i=0; i<this.curvature; i++) {
@@ -47,13 +67,49 @@ class SmokeBrush {
         }  
     }
 
+    colorCheck(){
+        if (document.getElementById('randomcolor').checked) 
+            return true;
+
+    }
+
+    strokeWghtCheck(){
+        if (document.getElementById('randomstrokeweight').checked) 
+            return true;
+
+    }
+
+    segLengthCheck(){
+        if (document.getElementById('randomseglength').checked) 
+            return true;
+
+    }
+
     segment(x, y, a) {
-        strokeWeight(1);
-        stroke(this.colour);
+        if (this.strokeWghtCheck() != true) {
+            strokeWeight(this.strokeWght); 
+        }
+        else {
+            let randomstrokeweight = Math.random()*9;
+            strokeWeight(randomstrokeweight);
+        }
+        if (this.colorCheck() != true) {
+            stroke(this.colour); 
+        }
+        else {
+            let randomhex = '#000000'.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);} );
+            stroke(randomhex);
+        }
         push();
         translate(x, y);
         rotate(a);
-        line(0, 0, this.segLength, 0);
+        if (this.segLengthCheck() != true) {
+            line(0, 0, this.segLength, 0);
+        }
+        else {
+            let randomseglength = Math.random()*80;
+            line(0, 0, randomseglength, 0);
+        } 
         pop();
     }
 
@@ -61,28 +117,41 @@ class SmokeBrush {
         let dx = xin - this.x[i];
         let dy = yin - this.y[i];
         let angle = atan2(dy, dx);  
-        this.x[i] = xin - cos(angle) * this.segLength;
-        this.y[i] = yin - sin(angle) * this.segLength;
+        this.x[i] = xin - cos(angle+this.xcurve) * this.segLength;
+        this.y[i] = yin - sin(angle+this.ycurve) * this.segLength;
         return this.segment(this.x[i], this.y[i], angle);
     }
 
-    draw() {
-        if(mouseIsPressed && mouseButton == LEFT){
-            let PX = mouseX;
-            let PY= mouseY;
-            this.dragSegment(0, PX, PY);
-            for(let i=0; i<this.x.length-1; i++) {
-                this.dragSegment(i+1, this.x[i], this.y[i]);
+    draw(g) {
+        if(g === undefined){
+            if(mouseIsPressed && mouseButton == LEFT){
+                let PX = mouseX;
+                let PY= mouseY;
+                this.dragSegment(0, PX, PY);
+                for(let i=0; i<this.x.length-1; i++) {
+                    this.dragSegment(i+1, this.x[i], this.y[i]);
+                }
             }
+            if(mouseIsPressed && mouseButton == RIGHT){
+                background('#000000');  
+            } 
         }
-        if(mouseIsPressed && mouseButton == RIGHT){
-            background('#000000');  
-        }   
-    }
-  
+    
+        else {
+            if(mouseIsPressed && mouseButton == LEFT){  
+                let PX = mouseX;
+                let PY= mouseY;
+                g.this.dragSegment(0, PX, PY);
+                for(let i=0; i<this.x.length-1; i++) {
+                    g.this.dragSegment(i+1, this.x[i], this.y[i]);
+                } 
+            }
+            if(mouseIsPressed && mouseButton == RIGHT){
+                background('#000000');  
+            } 
+        }
+    } 
 }
-
-
 
 /*
  "Smoke Brush" by Laura Valentini
