0c1b5c8987f02c17084b15e7851a67bbbe1a35de
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-01-15 23:50:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,43 @@ let art;
 
 function setup() {
     createCanvas(windowWidth,windowHeight); 
-    background('#000000');
+    background('#000000');  
     art = new SmokeBrush(); 
     art.iterate();
+}     
+       
 
+function draw() {   
+    art.draw();
 }
 
-function draw(obj) {
-    if(obj){
-        obj.art.draw();
-    }
-    else {
-        art.draw();
-    }
-}
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+
     let cc = document.getElementById('colour');
-    function changeColour(event){
+    function changeColour(){
         let colour = document.getElementById('colour').value;
         art.setColour(colour);
     }
 
-
     let sl = document.getElementById('segLength');
-    function changeSegLength(event){
+    function changeSegLength(){
         let segLength = document.getElementById('segLength').value;
         art.setSegmentLength(segLength);
     }
-    
+
+    let sw = document.getElementById('strokeWght');
+    function changeStrokeWght(){
+        let strokeWght = document.getElementById('strokeWght').value;
+        art.setStrokeWght(strokeWght);
+    }
+
+
     cc.addEventListener('change', changeColour);
     sl.addEventListener('input', changeSegLength);
- 
+    sw.addEventListener('input', changeStrokeWght);
+   
+    
 
     let customize = document.getElementById('customize');
 
